351a1d27142f1d1a9ffe9dc5848abd275c38603a
k0rabl <theevil228@gmail.com>
2019-10-28 13:53:21 +0200

    Bugfix: mouse generation on the snake. A new biggest field.  Add comments.

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-var field,
-		coordSnake,
+var field,			
 		mouse,
 		snake,
 		interval,
 		score,
 		direction = 'right';
 
-function randIntExcep(min, max, exp) {
-			var n;
-			while(true){
-					if((n = Math.floor(Math.random() * (max - min + 1)) + min) != exp)
-					return n;
-			}
-}
-
 function startGame(){
-	genField();
-	createSnake();
-	createMouse();
-	interval = setInterval(move, 70);
-	startBlock.classList.add('disable');
+	game.classList.remove('disable');														// enable game-score
+	genField();																									// generation a new game field
+	createSnake();																							// generation a new snake
+	createMouse();																							// generation a new mouse
+	interval = setInterval(move, 70);														// start moving and sеt complexity
+	startBlock.classList.add('disable');												// disabled game-menu
 }
 
 function endGame(){
-	score = (snake.length - 3);
-	alert('You lose. Score:' + score);
-	clearInterval(interval);
-	field.remove();
-	document.getElementById("highscore").textContent = score;
-	startBlock.classList.remove('disable');
+	score = (snake.length - 3);																	// count a score
+	alert('You lose. Score:' + score);													// lose nameplate 
+	clearInterval(interval);																		// clear interval for a new game
+	field.remove();																							// clear field
+	document.getElementById("highscore").textContent = score;		// set a highscore
+	yourScore.textContent = 0; 																	// clear score
+	startBlock.classList.remove('disable');											// enable game menu
+	game.classList.add('disable');															// disable game-score
 }
 
 function genField(){
 
+	// generation a field
 	field  = document.createElement('div');
 	document.body.appendChild(field);
 	field.classList.add('field');
 
-	for (let i=0; i<2501; i++){
+	// generation a 1250 excels
+	for (let i=0; i<1250; i++){
 		let excel = document.createElement('div');
 		field.appendChild(excel);
 		excel.classList.add('excel'); 
 	}
 
 	var x = 1,
-			y = 50;
+			y = 25;
 
-	for	(let i=0; i<2501; i++){
+	// set excel coordination
+	for	(let i=0; i<1250; i++){
+		// transfer excel x on a new line
 		if (x>50){
 			x = 1;
 			y--;
@@ -59,13 +57,16 @@ function genField(){
 }
 
 function createSnake(){
+
+	// generation a random coordination of snake
 	function genSnake(){
 		let posX = Math.round(Math.random() * (50 - 3) + 3);
-		let posY = Math.round(Math.random() * (50 - 1) + 1);
+		let posY = Math.round(Math.random() * (25 - 1) + 1);
 		return [posX, posY];
 	}
-	
-	coordSnake = genSnake();
+
+	//create snake
+	let coordSnake = genSnake();
 	snake = [document.querySelector('[posX = "' + coordSnake[0] + '"][posY = "' + coordSnake[1] + '"]'),
 							 document.querySelector('[posX = "' + (coordSnake[0]-1) + '"][posY = "' + coordSnake[1] + '"]'),
 							 document.querySelector('[posX = "' + (coordSnake[0]-2) + '"][posY = "' + coordSnake[1] + '"]')];
@@ -73,29 +74,42 @@ function createSnake(){
 	for (i=0; i<snake.length; i++){
 		snake[i].classList.add('excel-snake');
 	}
-	
+
+	//gen a snake head
 	snake[0].classList.add('head');
 }
 
 function createMouse(){
+
+	// generation a random coordination of snake
 	function genMouse(){
-		let posX = randIntExcep((50-1), 1, coordSnake[0]);
-		let posY = randIntExcep((50-1), 1, coordSnake[1]);
+		let posX = Math.round(Math.random() * (50 - 1) + 1);
+		let posY = Math.round(Math.random() * (25 - 1) + 1);
 		return[posX, posY];
 	}
 	
+	// create mouse
 	let coordMouse = genMouse();
-	mouse = [document.querySelector('[posX = "' + coordMouse[0] + '"][posY = "' + coordMouse[1] + '"]')]
+	mouse = [document.querySelector('[posX = "' + coordMouse[0] + '"][posY = "' + coordMouse[1] + '"]')];
 	
+	// if mouse creating on snake
+	while(mouse[0].classList.contains('excel-snake')){
+		let coordMouse = genMouse();	
+		mouse = [document.querySelector('[posX = "' + coordMouse[0] + '"][posY = "' + coordMouse[1] + '"]')];
+	}
+
 	mouse[0].classList.add('mouse')
 }
 
 function move(){
-	let snakeCoord = [snake[0].getAttribute('posX'), snake[0].getAttribute('posY')];
-	snake[0].classList.remove('head');
-	snake[snake.length - 1].classList.remove('excel-snake');
+
+	//snake moving
+	let snakeCoord = [snake[0].getAttribute('posX'), snake[0].getAttribute('posY')]; //coordination of snake head 
+	snake[0].classList.remove('head');	//clear head 
+	snake[snake.length - 1].classList.remove('excel-snake'); //clear tail
 	snake.pop();
 
+	// button movement, if snake beyond the limits, than snake becomeon the start line
 	if (direction == 'right'){
 		if(snakeCoord[0] < 50){
 			snake.unshift(document.querySelector('[posX = "' + (+snakeCoord[0] + 1) + '"][posY = "' + snakeCoord[1] + '"]'));
@@ -109,7 +123,7 @@ function move(){
 			snake.unshift(document.querySelector('[posX = "50"][posY = "' + snakeCoord[1] + '"]'));
 		}
 	} else if (direction == 'up'){
-		if(snakeCoord[1] < 50){
+		if(snakeCoord[1] < 25){
 			snake.unshift(document.querySelector('[posX = "' + snakeCoord[0] + '"][posY = "' + (+snakeCoord[1]+  1) + '"]'));
 		} else {
 			snake.unshift(document.querySelector('[posX = "' + snakeCoord[0] + '"][posY = "1"]'));
@@ -118,10 +132,11 @@ function move(){
 		if(snakeCoord[1] > 1){
 			snake.unshift(document.querySelector('[posX = "' + snakeCoord[0] + '"][posY = "' + (+snakeCoord[1] - 1) + '"]'));
 		} else {
-			snake.unshift(document.querySelector('[posX = "' + snakeCoord[0] + '"][posY = "50"]'));
+			snake.unshift(document.querySelector('[posX = "' + snakeCoord[0] + '"][posY = "25"]'));
 		}
 	}
 
+	//snake eat mouse
 	if (snake[0].getAttribute('posX') == mouse[0].getAttribute('posX') &&
 		  snake[0].getAttribute('posY') == mouse[0].getAttribute('posY')){
 		
@@ -131,15 +146,17 @@ function move(){
 				let y = snake[snake.length - 1].getAttribute('posY');
 				snake.push(document.querySelector('[posX = "' + x + '"][posY = "' + y + '"]'));
 				
-				yourScore.textContent = (snake.length - 3); 
+				yourScore.textContent = (snake.length - 3); 	//update score
 
 				createMouse();
 	}
 
+	//snake eat tail
 	if (snake[0].classList.contains('excel-snake')){
 		endGame();
 	}
 
+	//rise snake
 	snake[0].classList.add('head');
 	for (i=0; i < snake.length; i++) {
 		snake[i].classList.add('excel-snake');
@@ -148,14 +165,12 @@ function move(){
 
 }
 
-
-
+// press "Start" for play a game
 start.onclick = function(e) {
-	startGame();
+	startGame();	
 }
 
-
-
+// move button 
 window.addEventListener('keydown', function(e){ 
 	if (e.keyCode == 37 && direction != 'right'){
 		direction = 'left';
